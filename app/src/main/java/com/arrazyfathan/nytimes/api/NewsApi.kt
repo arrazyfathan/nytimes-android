681abaec3d353def49108dd212a38733d5cd601b
ar razy fathan rabbani <arrazy.rabbani266@gmail.com>
2022-04-08 06:41:23 +0700

    setup project - add dependencies

---
+++ app/src/main/java/com/arrazyfathan/nytimes/api/NewsApi.kt
@@ -0,0 +1,11 @@
+package com.arrazyfathan.nytimes.api
+
+import androidx.viewbinding.BuildConfig
+
+interface NewsApi {
+
+    companion object {
+        const val BASE_URL = "https://api.nytimes.com/svc/topstories/v2/"
+    }
+
+}
---
+++ app/src/main/java/com/arrazyfathan/nytimes/data/model/TopStories.kt
@@ -0,0 +1,5 @@
+package com.arrazyfathan.nytimes.data.model
+
+data class TopStories(
+    val results: List<Article>,
+)
